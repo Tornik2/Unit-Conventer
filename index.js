6a52fc6b48c9ac0ef5411d18fa847290ae6bc55c
Tornik2 <tornik2@yahoo.com>
2023-02-11 10:51:53 +0400

    Update index,js

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const metP = document.getElementById("met-p")
 const litP = document.getElementById("lit-p")
 const kilP = document.getElementById("kil-p")
 const Btn = document.getElementById("btn")
-inpText.value = 0
+inpText.value = "0"
 btn.addEventListener("click", function() {
-    metP.textContent = `${inpText.value} meters = ${Math.round(inpText.value * 3.281*100)/100} feet | ${inpText.value} feet = ${Math.round(inpText.value * 0.304 *100)/100} meters`
-    litP.textContent = `${inpText.value} liters = ${Math.round(inpText.value * 0.264 *100)/100} gallons | ${inpText.value} gallons = ${Math.round(inpText.value * 3.785 *100)/100} liters`
-    kilP.textContent = `${inpText.value} kilos = ${Math.round(inpText.value * 2.204 *100)/100} pounds | ${inpText.value} pounds = ${Math.round(inpText.value * 0.453592*100)/100} kilos`
+    metP.textContent = `${inpText.value} meters = ${Math.round(inpText.value * 3.281*100)/100} feet 
+    | ${inpText.value} feet = ${Math.round(inpText.value * 0.304 *100)/100} meters`
+    litP.textContent = `${inpText.value} liters = ${Math.round(inpText.value * 0.264 *100)/100} gallons 
+    | ${inpText.value} gallons = ${Math.round(inpText.value * 3.785 *100)/100} liters`
+    kilP.textContent = `${inpText.value} kilos = ${Math.round(inpText.value * 2.204 *100)/100} pounds 
+    | ${inpText.value} pounds = ${Math.round(inpText.value * 0.453592*100)/100} kilos`
 })
 
